c91655d69287a1eaecbd685f4994d7b391672208
LE-Smith <djsschmidt@googlemail.com>
2021-02-08 11:28:05 +0100

    add quote-component functionality

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React from 'react';
 import './Quote.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from 'react-bootstrap/Button';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Quotes = props => {
+  const styles = {
+    btn: {
+      backgroundColor: props.color,
+      border: 'none',
+    },
+    text: {
+      color: props.color,
+    },
+  };
 
-
-    return (
-        <div id="quote-box">
-            <p id="text"><i class="fa fa-quote-left"></i>{props.text}</p>
-            <p id="author">{props.author}</p>
-            <button id="new-quote">New Quote</button>
-        </div>
-    )
-
-} 
+  return (
+    <div id="quote-box">
+      <p style={styles.text} id="text">
+        <FontAwesomeIcon className="fa-icon" icon={faQuoteLeft} />
+        {props.text}
+      </p>
+      <p style={styles.text} id="author">
+        - {props.author}
+      </p>
+      <div id="buttons">
+        <Button
+          className="bootstrap-btn"
+          style={styles.btn}
+          id="new-quote"
+          onClick={props.twitterClicked}
+        >
+          <FontAwesomeIcon className="fa-icon" icon={faTwitter} />
+        </Button>
+        <Button
+          className="bootstrap-btn"
+          style={styles.btn}
+          id="new-quote"
+          onClick={props.newQuoteClicked}
+        >
+          New Quote
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default Quotes;
