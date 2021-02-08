eeba086ecda2e83ebbd07a6e62940c488cf57a84
LE-Smith <djsschmidt@googlemail.com>
2021-02-08 09:49:24 +0100

    add quote component

---
+++ src/components/Quotes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Quotes.scss';
+
+const Quotes = props => {
+
+
+    return (
+        <div id="quote-box">
+            <p id="text"><i class="fa fa-quote-left"></i>{props.text}</p>
+            <p id="author">{props.author}</p>
+            <button id="new-quote">New Quote</button>
+        </div>
+    )
+
+} 
+
+export default Quotes;
