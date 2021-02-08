256f8d6a231436eb4fad1e6238a50b7fe03af892
LE-Smith <djsschmidt@googlemail.com>
2021-02-08 09:49:05 +0100

    add quotecontainer

---
+++ src/containers/QuoteContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Quotes from '../components/Quotes';
+
+const QuoteContainer = props => {
+    return (
+        <Quotes text="We become what we think about." author="Earl Nightingale"/>
+    )
+}
+
+export default QuoteContainer;
